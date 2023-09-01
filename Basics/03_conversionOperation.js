7da64666d7d60f4d1ef28837c2918f0cb9dc6129
sujeetgithubweb <sujeetsujeet130@gmail.com>
2023-09-01 18:07:56 +0530

    convesion and comperion donne

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -27,3 +27,35 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 
 console.log(typeof stringNumber);
+
+
+// **********************  Operations  *************************
+
+let value = 3
+let negValuev=-value
+// console.log(negValuev);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "sujeet "
+let str2 = "kumar"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2" + "2");
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 22+4
+
